6ca54bda5f5a753b32ee10a2229d2a7d175fd769
emilia grama <emiliagrama@gmail.com>
2026-04-16 14:07:11 +0200

    Support comment creation with required review fields

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,28 @@
-class Api::CommentsController < ApplicationController
+module Api
+  class CommentsController < ApplicationController
+    skip_before_action :authorize_request, only: [:index, :create]
+
+    def index
+      review_session = ReviewSession.find(params[:review_session_id])
+      comments = review_session.comments.order(created_at: :asc)
+      render json: comments
+    end
+
+    def create
+      review_session = ReviewSession.find(params[:review_session_id])
+      comment = review_session.comments.build(comment_params)
+
+      if comment.save
+        render json: comment, status: :created
+      else
+        render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def comment_params
+      params.permit(:author_name, :body, :page_url, :x_percent, :y_percent)
+    end
+  end
 end
