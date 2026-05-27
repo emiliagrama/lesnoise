a8d6efea39bc704db8ea22efa0a4b3889e683a9a
emilia grama <emiliagrama@gmail.com>
2026-05-27 13:58:39 +0200

    Unresolved badge on dashboard

--- app/controllers/api/review_sessions_controller.rb
+++ app/controllers/api/review_sessions_controller.rb
@@ -1,8 +1,13 @@
 module Api
   class ReviewSessionsController < ApplicationController
     def index
-      review_sessions = current_user.projects.includes(:review_sessions).flat_map(&:review_sessions)
-      render json: review_sessions
+      review_sessions = current_user.projects.includes(review_sessions: :comments).flat_map(&:review_sessions)
+
+      render json: review_sessions.map { |review_session|
+        review_session.as_json.merge(
+          unresolved_comments_count: review_session.comments.where(resolved: false).count
+        )
+      }
     end
 
     def create
@@ -10,11 +15,13 @@ module Api
 
       review_session = project.review_sessions.build(review_session_params)
 
-      if review_session.save
-        render json: review_session, status: :created
-      else
-        render json: { errors: review_session.errors.full_messages }, status: :unprocessable_entity
-      end
+    if review_session.save
+      render json: review_session.as_json.merge(
+        unresolved_comments_count: 0
+      ), status: :created
+    else
+      render json: { errors: review_session.errors.full_messages }, status: :unprocessable_entity
+    end
     end
 
     def show
