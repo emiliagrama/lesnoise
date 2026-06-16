26fffe1575a5dae1d57431a996b72b5d6e5e98c9
emilia grama <emiliagrama@gmail.com>
2026-06-16 14:42:23 +0200

    Fix slug generation

--- app/models/review_session.rb
+++ app/models/review_session.rb
@@ -5,7 +5,7 @@ class ReviewSession < ApplicationRecord
   before_validation :set_share_token, on: :create
   before_validation :generate_slug, on: :create
 
-  validates :name, :base_url, presence: true, length: { maximum: 30 }
+  validates :name, :base_url, presence: true, length: { maximum: 50 }
   validates :slug, presence: true, uniqueness: true
 
   validate :name_unique_for_user
